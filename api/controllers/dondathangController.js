c803883812f0a123f019dcdbabbaf769deca5cbe
Hungkudo <89557275+Hungkudo@users.noreply.github.com>
2023-12-05 21:54:54 +0700

    them trang thai vao dondathang

--- api/controllers/dondathangController.js
+++ api/controllers/dondathangController.js
@@ -3,75 +3,121 @@
 const util = require('util');
 const mysql = require('mysql');
 const db = require('../db');
-
 // Sửa dondathang với cập nhật tự động tongtien khi soluong thay đổi
 exports.Editdondathang = function (req, res) {
-    // Lấy giá của sản phẩm từ bảng sách
-    let getProductPriceSQL = 'SELECT gia FROM sach WHERE masp = ?';
-    db.query(getProductPriceSQL, [req.body.masp], (err, productInfo) => {
+    // Kiểm tra xem makh có tồn tại không
+    let checkCustomerSQL = 'SELECT * FROM khachhang WHERE makh = ?';
+    db.query(checkCustomerSQL, [req.body.makh], (err, customerInfo) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ message: 'Internal Server Error' });
         }
 
-        // Kiểm tra xem sách có tồn tại không
-        if (!productInfo || productInfo.length === 0) {
-            return res.status(404).json({ message: 'Sản phẩm không tồn tại' });
+        if (!customerInfo || customerInfo.length === 0) {
+            return res.status(404).json({ message: 'Không tìm thấy khách hàng có mã này' });
         }
 
-        // Tính tổng tiền dựa trên giá mới và số lượng mới của sách
-        const newTotalAmount = req.body.soluong * productInfo[0].gia;
+        // Kiểm tra xem masp có tồn tại không
+        let checkProductSQL = 'SELECT * FROM sach WHERE masp = ?';
+        db.query(checkProductSQL, [req.body.masp], (err, productInfo) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
 
-        // Sửa đơn đặt hàng trong cơ sở dữ liệu với cập nhật tự động tongtien
-        let updateOrderSQL = 'UPDATE dondathang SET makh=?, masp=?, soluong=?, tongtien=? WHERE madh=?';
-        db.query(
-            updateOrderSQL,
-            [req.body.makh, req.body.masp, req.body.soluong, newTotalAmount, req.params.madh],
-            (err, response) => {
+            if (!productInfo || productInfo.length === 0) {
+                return res.status(404).json({ message: 'Không tìm thấy sản phẩm có mã này' });
+            }
+
+            // Lấy giá của sản phẩm từ bảng sách
+            let getProductPriceSQL = 'SELECT gia FROM sach WHERE masp = ?';
+            db.query(getProductPriceSQL, [req.body.masp], (err, productInfo) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).json({ message: 'Internal Server Error' });
                 }
 
-                res.json(response);
-            }
-        );
+                // Kiểm tra xem sách có tồn tại không
+                if (!productInfo || productInfo.length === 0) {
+                    return res.status(404).json({ message: 'Sản phẩm không tồn tại' });
+                }
+
+                // Tính tổng tiền dựa trên giá mới và số lượng mới của sách
+                const newTotalAmount = req.body.soluong * productInfo[0].gia;
+
+                // Sửa đơn đặt hàng trong cơ sở dữ liệu với cập nhật tự động tongtien
+                let updateOrderSQL = 'UPDATE dondathang SET makh=?, masp=?, soluong=?, tongtien=?, trangthai=?, pttt=? WHERE madh=?';
+                db.query(
+                    updateOrderSQL,
+                    [req.body.makh, req.body.masp, req.body.soluong, newTotalAmount, req.body.trangthai, req.body.pttt, req.params.madh],
+                    (err, response) => {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ message: 'Internal Server Error' });
+                        }
+
+                        res.json(response);
+                    }
+                );
+            });
+        });
     });
 };
 
 
 // Thêm dondathang
 exports.Insertdondathang = function (req, res) {
-    // Lấy giá và thông tin khác của sách từ bảng sách
-    let getProductInfoSQL = 'SELECT gia FROM sach WHERE masp = ?';
-    db.query(getProductInfoSQL, [req.body.masp], (err, productInfo) => {
+    // Kiểm tra xem makh có tồn tại không
+    let checkCustomerSQL = 'SELECT * FROM khachhang WHERE makh = ?';
+    db.query(checkCustomerSQL, [req.body.makh], (err, customerInfo) => {
         if (err) {
             console.error(err);
             return res.status(500).json({ message: 'Internal Server Error' });
         }
 
-        // Kiểm tra xem sách có tồn tại không
-        if (!productInfo || productInfo.length === 0) {
-            return res.status(404).json({ message: 'Sản phẩm không tồn tại' });
+        if (!customerInfo || customerInfo.length === 0) {
+            return res.status(404).json({ message: 'Không tìm thấy khách hàng có mã này' });
         }
 
-        // Tính tổng tiền dựa trên giá và số lượng của sách
-        const totalAmount = req.body.soluong * productInfo[0].gia;
+        // Kiểm tra xem masp có tồn tại không
+        let checkProductSQL = 'SELECT * FROM sach WHERE masp = ?';
+        db.query(checkProductSQL, [req.body.masp], (err, productInfo) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
 
-        // Thêm đơn đặt hàng vào cơ sở dữ liệu với tổng tiền đã tính
-        let insertOrderSQL = 'INSERT INTO dondathang VALUES (NULL, ?, ?, ?, ?)';
-        db.query(
-            insertOrderSQL,
-            [req.body.makh, req.body.masp, req.body.soluong, totalAmount],
-            (err, response) => {
+            if (!productInfo || productInfo.length === 0) {
+                return res.status(404).json({ message: 'Không tìm thấy sản phẩm có mã này' });
+            }
+
+            // Nếu cả makh và masp đều tồn tại, tiếp tục xử lý và thêm đơn đặt hàng
+            let getProductInfoSQL = 'SELECT gia FROM sach WHERE masp = ?';
+            db.query(getProductInfoSQL, [req.body.masp], (err, productInfo) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).json({ message: 'Internal Server Error' });
                 }
 
-                res.json(response);
-            }
-        );
+                // Tính tổng tiền dựa trên giá và số lượng của sách
+                const totalAmount = req.body.soluong * productInfo[0].gia;
+
+                // Thêm đơn đặt hàng vào cơ sở dữ liệu với tổng tiền đã tính
+                let insertOrderSQL = 'INSERT INTO dondathang VALUES (NULL, ?, ?, ?, ?, ?, ?)';
+                db.query(
+                    insertOrderSQL,
+                    [req.body.makh, req.body.masp, req.body.soluong, totalAmount, req.body.trangthai, req.body.pttt],
+                    (err, response) => {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).json({ message: 'Internal Server Error' });
+                        }
+
+                        res.json(response);
+                    }
+                );
+            });
+        });
     });
 };
 
